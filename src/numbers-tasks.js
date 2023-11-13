3467c0d4f073da2c6879734f6549c189c0f72cc6
Andrii <andriyshopyak@gmail.com>
2023-11-13 21:41:46 +0200

    feat: add solution for isNumber task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -441,8 +441,9 @@ function getNumberValue(number) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  // eslint-disable-next-line use-isnan
+  return Number.isFinite(number);
 }
 
 /**
