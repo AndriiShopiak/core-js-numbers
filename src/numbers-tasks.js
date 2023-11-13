f98bb8f60b14d2de147a91633d18ef5755bf34d7
Andrii <andriyshopyak@gmail.com>
2023-11-13 20:18:11 +0200

    feat: add solution for getNumberValue task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -421,8 +421,9 @@ function toPrecision(number, precision) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  // eslint-disable-next-line no-new-wrappers
+  return number.valueOf();
 }
 
 /**
